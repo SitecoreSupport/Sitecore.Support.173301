73d9f1a78c802503c3f00dc4ccfc30478c4340c7
ArtemNizelnyk <arn@sitecore.net>
2018-07-25 18:33:49 +0300

    Initial Commit

---
+++ src/Sitecore.Support.173301/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.173301")]
+[assembly: AssemblyProduct("Sitecore.Support.173301")]
+[assembly: ComVisible(false)]
