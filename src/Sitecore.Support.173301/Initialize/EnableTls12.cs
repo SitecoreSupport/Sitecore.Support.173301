f41c0f07b1de10fb8154687c34d51b83931e70c7
ArtemNizelnyk <arn@sitecore.net>
2018-07-25 18:44:54 +0300

    Sets TLS1.2 as a default protocol on application startup

---
+++ src/Sitecore.Support.173301/Initialize/EnableTls12.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.Support.Initialize
+{
+  using System.Net;
+  using Pipelines;
+
+  public class EnableTls12
+  {
+    public void Process(PipelineArgs args)
+    {
+      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+    }
+  }
+}
